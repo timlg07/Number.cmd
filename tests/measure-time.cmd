5bc229bf313f7f0adb072c03982efa2f704dfb89
Tim Greller <timlg07@gmail.com>
2020-10-15 18:23:19 +0200

    Adjusted paths in time-measure script.

--- tests/measure-time.cmd
+++ tests/measure-time.cmd
@@ -2,6 +2,6 @@
 :main
     set /p "term=Number::measureTime> "
     echo;|set/p "=result: "
-    call "%~dp0Number.cmd" # %term%
-    powershell \"time: \" + (Measure-Command {"%~dp0Number" _ %term%}).TotalMilliseconds.ToString() + \"ms`r`n\"
+    call "%~dp0..\Number.cmd" # %term%
+    powershell \"time: \" + (Measure-Command {"%~dp0..\Number" _ %term%}).TotalMilliseconds.ToString() + \"ms`r`n\"
 goto main
