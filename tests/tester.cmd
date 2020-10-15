8312b952fae7bb5e69b166617dfca22207c8037d
Tim Greller <timlg07@gmail.com>
2020-10-15 21:35:14 +0200

    Added simple tester.

---
+++ tests/tester.cmd
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+if not exist "%~1" (
+    echo ERROR. No valid test-file provided.
+    echo Required parameter:
+    echo   A file containing pairs of parameters and their expected output.
+    echo   Both strings should be divided by an equal sign.
+    echo   Example line: `5 * 7 = +35E0`
+)
+
+
+set /a total=failed=passed=0
+
+echo.
+echo:------------------
+echo:--- Starting tests: %~n1
+echo:------------------
+echo.
+
+for /F "usebackq tokens=1* delims==" %%P in ("%~1") do (
+    for /F "usebackq" %%R in (`..\Number # %%P`) do (
+        for /F "tokens=* delims= " %%E in ("%%Q") do (
+            if "%%R"=="%%E" (
+                echo:    test passed: %%P = %%E
+                set /a passed += 1
+            ) else (
+                echo:[!] test failed: %%P = %%E
+                set /a failed += 1
+            )
+            set /a total += 1
+        )
+    )
+)
+
+echo.
+echo:------------------
+echo:--- Tests finished
+echo:--- Total tests:  %total%
+echo:--- Passed tests: %passed%
+echo:--- Failed tests: %failed%
+echo:------------------
+echo.
+
+pause
