ac272e03e15b9f8caf9d30c4ce513f55177236f9
Tim Greller <timlg07@gmail.com>
2020-10-23 03:03:24 +0200

    Improved the I/O of the circle area example.

--- examples/circle area.cmd
+++ examples/circle area.cmd
@@ -3,14 +3,14 @@ cd ..
 title Circle Area Calculator - Number.cmd Demo
 
 :main
-echo:Radius of the circle:
-set /p "r= > "
+set /p "r=Radius of the circle: "
 
 rem calculating the area: A = pi * r^2
 call Number r_squared = %r% * %r%
 call Number A = Number.pi * %r_squared%
 
-echo:The area of the circle is: %A%.
-echo:(A = pi * r^2 = pi * %r_squared% = %A%)
+echo:The area A of the circle is: %A%.
+echo:A = pi * %r%^^2 = pi * %r_squared% = %A%
 pause
+echo:
 goto main
