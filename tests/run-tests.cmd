0af3f59a9e83e2af339925e9c0d31a28e82260ea
Tim Greller <timlg07@gmail.com>
2020-11-10 19:41:54 +0100

    Automated tests: Added functionality to measure the duration of each test.

--- tests/run-tests.cmd
+++ tests/run-tests.cmd
@@ -1,9 +1,11 @@
 @echo off
-set /a total=failed=passed=files=0
+set /a total=failed=passed=files=time=0
 echo Running all tests in %cd%
 for %%T in ("%cd%\*.test") do call :test "%%~fT"
 
 if %failed% equ 0 color a0 
+
+set /a avgTime = time / files
 
 echo.
 echo:------------------------------
@@ -11,6 +13,7 @@ echo:--- Test files executed: %files%
 echo:---         Total tests: %total%
 echo:---        Passed tests: %passed%
 echo:---        Failed tests: %failed%
+echo:---        Average time: %avgTime% ms
 echo:------------------------------
 echo.
 
@@ -19,9 +22,10 @@ exit
 
 
 :test
-    call "%~dp0tester.cmd" "%~1" || color c0
+    call "%~dp0tester.cmd" "%~1" 1 || color c0
     set /a  files += 1
     set /a  total += _total
     set /a failed += _failed
     set /a passed += _passed
+    set /a   time += _averageTime
 exit /b %errorlevel%
--- tests/tester.cmd
+++ tests/tester.cmd
@@ -8,18 +8,28 @@ if not exist "%~1" (
     echo   Example line: `5 * 7 = +35E0`
 )
 
+set /a _measureTime = 0
+if "%~2" neq "" set "_measureTime=%~2"
 
-set /a _total=_failed=_passed=0
+set /a _total = _failed = _passed = _totalTime = 0
 
 echo.
 echo:--- Starting tests: %~n1
 echo.
 
 for /F "usebackq tokens=1* delims==" %%P in ("%~1") do (
-    for /F "usebackq" %%R in (`..\Number # %%P`) do (
+    for /F "usebackq" %%R in (`%~dp0..\Number # %%P`) do (
         for /F "tokens=* delims= " %%E in ("%%Q") do (
             if "%%R"=="%%E" (
-                echo:[ ] test passed: %%P = %%E
+                if %_measureTime% neq 0 (
+                    for /F "usebackq tokens=1,2 delims=.," %%t in (`powershell -command "(Measure-Command {"%~dp0..\Number" _ %%P%}).TotalMilliseconds.ToString()"`) do (
+                        echo.[ ] test passed: %%P = %%E; completed in %%t,%%u ms
+                        set /a _totalTime += %%t
+                        2>nul (echo.%%P = %%E :: %%t,%%u ms >> "%~dp1performance\%~n1.times.log")
+                    )
+                ) else (
+                    echo:[ ] test passed: %%P = %%E
+                )
                 set /a _passed += 1
             ) else (
                 echo:[!] test failed: %%P; expected: %%E but was: %%R
@@ -30,11 +40,14 @@ for /F "usebackq tokens=1* delims==" %%P in ("%~1") do (
     )
 )
 
+set /a _averageTime = _totalTime / _passed
+
 echo.
 echo:--- Tests finished: %~n1
 echo:---    Total tests: %_total%
 echo:---   Passed tests: %_passed%
 echo:---   Failed tests: %_failed%
+echo:---   Average time: %_averageTime% ms
 echo.
 
 exit /b %_failed%
