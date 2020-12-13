e40597596ab976d46d60d8a9e046c351b8104e4b
Tim Greller <timlg07@gmail.com>
2020-12-13 13:06:18 +0100

    Extended example.cmd to fit the readme example.

--- examples/example.cmd
+++ examples/example.cmd
@@ -3,6 +3,10 @@
 call Number x = 4 + 48
 call Number x = %x% * -E5
 call Number y = 50E5 + 2E5
-call Number result = %x% + %y%
+call Number result = %x% - %y% 
+call Number result = %result% / 3000 
+call Number result = %result% + 4 "f:."
+
+echo result: %result%
 
 @pause
