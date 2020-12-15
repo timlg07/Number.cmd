0d0542dd686a04b117462156495bf52200b7097f
Tim Greller <timlg07@gmail.com>
2020-12-15 20:59:11 +0100

    Fixed bracket syntax bug in the tester.

--- tests/tester.cmd
+++ tests/tester.cmd
@@ -26,10 +26,10 @@ for /F "usebackq tokens=1* delims==" %%P in ("%~1") do (
         for /F "tokens=* delims= " %%E in ("%%Q") do (
             if "%%R"=="%%E" (
                 if %_measureTime% neq 0 (
-                    for /F "usebackq tokens=1,2 delims=.," %%t in (`powershell -command "(Measure-Command {"%~dp0..\Number" _ %%P%}).TotalMilliseconds.ToString()"`) do (
+                    for /F "usebackq tokens=1,2 delims=.," %%t in (`powershell -command "(Measure-Command {"%~dp0..\Number" _ %%P}).TotalMilliseconds.ToString()"`) do (
                         echo.[ ] test passed: %%P = %%E; completed in %%t,%%u ms
                         set /a _totalTime += %%t
-                        2>nul (echo.%%P = %%E :: %%t,%%u ms >> %_timeLogFile%)
+                        2>nul echo.%%P = %%E :: %%t,%%u ms >> %_timeLogFile%
                     )
                 ) else (
                     echo:[ ] test passed: %%P = %%E
