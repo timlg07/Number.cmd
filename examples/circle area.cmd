327178a54644141cbb9d40f39c87dba5dc8242d3
Tim Greller <timlg07@gmail.com>
2020-12-27 15:26:26 +0100

    Used the formatting option in the circle area example.

--- examples/circle area.cmd
+++ examples/circle area.cmd
@@ -7,9 +7,9 @@ set /p "r=Radius of the circle: "
 
 rem calculating the area: A = pi * r^2
 call Number r_squared = %r% * %r%
-call Number A = Number.pi * %r_squared%
+call Number A = Number.pi * %r_squared% format:.
 
-echo:The area A of the circle is: %A%.
+echo:The area A of the circle is: %A%
 echo:A = pi * %r%^^2 = pi * %r_squared% = %A%
 pause
 echo:
